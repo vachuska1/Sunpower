fa2d435af0ab421e73c94be6993e77c5b31d6a85
vachuska1 <aless.vachuska@seznam.cz>
2025-04-01 09:13:34 +0200

    Update .gitignore to ignore sensitive JSON file

---
+++ src/app/api/blog/route.ts
@@ -0,0 +1,19 @@
+import { NextResponse } from "next/server";
+
+export async function GET() {
+  const serviceAccount = {
+    type: "service_account",
+    project_id: process.env.GOOGLE_PROJECT_ID,
+    private_key_id: process.env.GOOGLE_PRIVATE_KEY_ID,
+    private_key: process.env.GOOGLE_PRIVATE_KEY?.replace(/\\n/g, "\n"),
+    client_email: process.env.GOOGLE_CLIENT_EMAIL,
+    client_id: process.env.GOOGLE_CLIENT_ID,
+    auth_uri: process.env.GOOGLE_AUTH_URI,
+    token_uri: process.env.GOOGLE_TOKEN_URI,
+    auth_provider_x509_cert_url: process.env.GOOGLE_AUTH_PROVIDER_CERT_URL,
+    client_x509_cert_url: process.env.GOOGLE_CLIENT_CERT_URL,
+    universe_domain: process.env.GOOGLE_UNIVERSE_DOMAIN,
+  };
+
+  return NextResponse.json({ message: "Success", serviceAccount });
+}
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,14 +1,10 @@
-import ProductList from './components/ProductList'
-import Blog from './components/Blog'
-import ContactForm from './components/ContactForm'
+
+
 
 const HomePage = () => {
   return (
     <div>
       <h1>Vítejte na stránkách Sunpower</h1>
-      <ProductList />
-      <Blog />
-      <ContactForm />
     </div>
   )
 }
